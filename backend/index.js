6ae577873944a2169e34609b8f044b527d8c7f9a
Deeptiranjan Mishra <124061435+DeeptiranjanMishra@users.noreply.github.com>
2025-04-30 01:46:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on port `)
+app.listen(process.env.PORT , () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
 })
 
 // import routes
